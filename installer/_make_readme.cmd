3542c86ddfc41a6c61c670923fcd1c846d2fbaee
nathancorvussolis <nathancorvussolis@gmail.com>
2016-02-15 10:46:26 +0900

    Make directory if not exist

--- installer/_make_readme.cmd
+++ installer/_make_readme.cmd
@@ -5,11 +5,13 @@ pushd "%~dp0"
 
 call _version.cmd
 
+if not exist "%TARGETDIR%" mkdir "%TARGETDIR%"
+
 pushd ..
 
 set DESCRIPTION=CorvusSKK
 
-"%LocalAppData%\Pandoc\pandoc.exe" -s -f markdown_github-ascii_identifiers -t html5 -V lang:"ja" -V title-prefix:"%DESCRIPTION%" -V pagetitle:"Manual" -V title:"%DESCRIPTION% Manual" --self-contained -c installer\resource-md\markdown.css --columns=1024 --toc -o installer\%TARGETDIR%\README.html README.md
+"%LocalAppData%\Pandoc\pandoc.exe" -s -f markdown_github-ascii_identifiers -t html5 -V lang:"ja" -V title-prefix:"%DESCRIPTION%" -V pagetitle:"Manual" -V title:"%DESCRIPTION% Manual" --self-contained -c "installer\resource-md\markdown.css" --columns=1024 --toc -o "installer\%TARGETDIR%\README.html" README.md
 
 popd
 
