d1a9129806be666e37a1af3d2b6d76e425a11432
nathancorvussolis <nathancorvussolis@gmail.com>
2016-06-09 16:18:17 +0900

    Add debug build batch file

---
+++ installer/_solution_clean_debug.cmd
@@ -0,0 +1,13 @@
+@echo off
+setlocal
+
+pushd "%~dp0"
+
+call "%VS140COMNTOOLS%VsDevCmd.bat"
+
+msbuild "..\imcrvskk.sln" /nologo /maxcpucount /verbosity:normal /target:Clean /property:Configuration=Debug,Platform=Win32
+msbuild "..\imcrvskk.sln" /nologo /maxcpucount /verbosity:normal /target:Clean /property:Configuration=Debug,Platform=x64
+
+popd
+
+endlocal
