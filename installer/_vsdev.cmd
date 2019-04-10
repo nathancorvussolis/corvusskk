a41eb776cfdfebc0daeaec8808236a41bc16339a
nathancorvussolis <nathancorvussolis@gmail.com>
2019-04-10 20:32:57 +0900

    Fixed VS installation path.

--- installer/_vsdev.cmd
+++ installer/_vsdev.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -requires Microsoft.Component.MSBuild -property installationPath`) do (
+for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -version ^[15.0^,16.0^) -requires Microsoft.Component.MSBuild -property installationPath`) do (
   if exist "%%i" (
     set VS2017InstallDir=%%i
   )
