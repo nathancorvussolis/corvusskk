5e6bd77458e698984f6c5a5055d3d64e5ebd0c4f
nathancorvussolis <nathancorvussolis@gmail.com>
2020-02-11 15:46:20 +0900

    Changed license according to original source license.

--- installer/_build_bundle.cmd
+++ installer/_build_bundle.cmd
@@ -11,7 +11,7 @@ call _version.cmd
 "%WIX%bin\light.exe" "%TARGETDIR%\corvusskk-%VERSION%.wixobj" -nologo -out "%TARGETDIR%\corvusskk-%VERSION%.exe" -ext WixBalExtension -ext WixUtilExtension
 
 if "%ENABLE_PLATFORM_ARM%" neq "0" (
-  "%WIX%bin\candle.exe" installer-arm-bundle.wxs -nologo -out "%TARGETDIR%\corvusskk-%VERSION%-arm.wixobj" -ext WixBalExtension -ext WixUtilExtension
+  "%WIX%bin\candle.exe" installer-bundle-arm.wxs -nologo -out "%TARGETDIR%\corvusskk-%VERSION%-arm.wixobj" -ext WixBalExtension -ext WixUtilExtension
   "%WIX%bin\light.exe" "%TARGETDIR%\corvusskk-%VERSION%-arm.wixobj" -nologo -out "%TARGETDIR%\corvusskk-%VERSION%-arm.exe" -ext WixBalExtension -ext WixUtilExtension
 )
 
