ba7253b0b41ac79d3e7f68104eaa35ed3b90d9db
nathancorvussolis <nathancorvussolis@gmail.com>
2021-12-05 15:01:41 +0900

    3.1.2
    * x86/x64版とARM版のインストーラーを統合しました。
    
    * 開発環境を Visual Studio 2022 に移行しました。
    
    * 候補一覧に表示される候補数を、7個固定から1～9個の任意の数を設定できるようにしました。
    
        * 設定ダイアログ「表示１」タブに「候補一覧表示に表示する候補数」の設定を追加しました。
    
        * 自前で候補を表示するアプリケーションでは、従来通りアプリケーション側が要求した数に従います。
    
    * 候補一覧選択キーの「表示」にUnicodeの拡張面の文字を使用可能としました。
    
    * 数値変換#4(数値再変換)に一部対応しました。
    
    * 再変換のキー設定を追加しました。
    
        * アプリケーション上で選択された文字列でユーザー辞書の送りなしエントリを逆引きしてから通常の辞書検索をおこないます。
    
        * 異なる見出し語に同じ候補が登録されている場合は、最も新しく確定された見出し語が適用されます。
    
    * 辞書管理プロセスのデバッグビルドで、通信スレッドによる描画更新のデッドロックを修正しました。
    
    * 設定ファイル、取込済SKK辞書ファイルの切り替え処理を改善しました。

--- common/version.h
+++ common/version.h
@@ -7,10 +7,10 @@
 #define TEXTSERVICE_DESC	TEXTSERVICE_NAME L"_DEBUG"
 #endif
 #define TEXTSERVICE_DIR		L"IMCRVSKK"
-#define TEXTSERVICE_VER		L"3.1.1"
+#define TEXTSERVICE_VER		L"3.1.2"
 
 //for resource
 #define RC_AUTHOR			"nathancorvussolis"
 #define RC_PRODUCT			"CorvusSKK"
-#define RC_VERSION			"3.1.1"
-#define RC_VERSION_D		3,1,1,0
+#define RC_VERSION			"3.1.2"
+#define RC_VERSION_D		3,1,2,0
--- installer/_version.cmd
+++ installer/_version.cmd
@@ -1,4 +1,4 @@
 
-set VERSION=3.1.1
+set VERSION=3.1.2
 
 set TARGETDIR=build
