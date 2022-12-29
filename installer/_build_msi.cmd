b4d2201b12c560efbdf2676323089b80b102a2ef
nathancorvussolis <nathancorvussolis@gmail.com>
2022-12-29 19:13:14 +0900

    Specified 32bit/64bit components with the candle command.

--- installer/_build_msi.cmd
+++ installer/_build_msi.cmd
@@ -11,29 +11,29 @@ call _build_sub.cmd
 
 rem x86
 
-"%WIX%bin\candle.exe" installer-x86.wxs ^
--nologo -out "%TARGETDIR%\x86.wixobj"
+"%WIX%bin\candle.exe" -nologo -arch x86 ^
+installer-x86.wxs -out "%TARGETDIR%\x86.wixobj"
 
-"%WIX%bin\light.exe" "%TARGETDIR%\x86.wixobj" ^
--nologo -out "%TARGETDIR%\x86.msi" -ext WixUIExtension -sw1076
+"%WIX%bin\light.exe" -nologo -ext WixUIExtension -sw1076 ^
+"%TARGETDIR%\x86.wixobj" -out "%TARGETDIR%\x86.msi"
 
 rem x64
 
-"%WIX%bin\candle.exe" installer-x64.wxs ^
--nologo -out "%TARGETDIR%\x64.wixobj"
+"%WIX%bin\candle.exe" -nologo -arch x64 ^
+installer-x64.wxs -out "%TARGETDIR%\x64.wixobj"
 
-"%WIX%bin\light.exe" "%TARGETDIR%\x64.wixobj" ^
--nologo -out "%TARGETDIR%\x64.msi" -ext WixUIExtension -sw1076
+"%WIX%bin\light.exe" -nologo -ext WixUIExtension -sw1076 ^
+"%TARGETDIR%\x64.wixobj" -out "%TARGETDIR%\x64.msi"
 
 rem ARM
 
-"%WIX%bin\candle.exe" installer-arm.wxs ^
--nologo -out "%TARGETDIR%\arm.wixobj"
+"%WIX%bin\candle.exe" -nologo -arch x64 ^
+installer-arm.wxs -out "%TARGETDIR%\arm.wixobj"
 
-"%WIX%bin\light.exe" "%TARGETDIR%\arm.wixobj" ^
--nologo -out "%TARGETDIR%\arm.msi" -ext WixUIExtension -sw1076
+"%WIX%bin\light.exe" -nologo -ext WixUIExtension -sw1076 ^
+"%TARGETDIR%\arm.wixobj" -out "%TARGETDIR%\arm.msi"
 
-msiinfo "%TARGETDIR%\arm.msi" /p Arm64;1033 /nologo
+msiinfo "%TARGETDIR%\arm.msi" /nologo /p Arm64;1033
 
 popd
 
