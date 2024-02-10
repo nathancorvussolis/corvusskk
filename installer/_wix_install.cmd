ccd1f5ce17b75db377409d6e98c6e4ee6d589bd1
nathancorvussolis <nathancorvussolis@gmail.com>
2024-02-11 00:28:09 +0900

    Installed/Updated all wix tool and extenstions.

--- installer/_wix_install.cmd
+++ installer/_wix_install.cmd
@@ -3,22 +3,31 @@ setlocal
 
 pushd "%~dp0"
 
-if not exist "%USERPROFILE%\.dotnet\tools\.store\wix" (
-  dotnet tool install --global wix
-)
+dotnet tool update --global wix
 
 echo [Tool]
 wix --version
 
 set EXTENSIONS= ^
 WixToolset.Bal.wixext ^
+WixToolset.ComPlus.wixext ^
+WixToolset.Dependency.wixext ^
+WixToolset.DifxApp.wixext ^
+WixToolset.DirectX.wixext ^
+WixToolset.Firewall.wixext ^
+WixToolset.Http.wixext ^
+WixToolset.Iis.wixext ^
+WixToolset.Msmq.wixext ^
+WixToolset.Netfx.wixext ^
+WixToolset.PowerShell.wixext ^
+WixToolset.Sql.wixext ^
 WixToolset.UI.wixext ^
-WixToolset.Util.wixext
+WixToolset.Util.wixext ^
+WixToolset.VisualStudio.wixext
 
 for %%i in (%EXTENSIONS%) do (
-  if not exist "%USERPROFILE%\.wix\extensions\%%i" (
-    wix extension add --global %%i
-  )
+  wix extension remove --global %%i
+  wix extension add --global %%i
 )
 
 echo [Extensions]
--- installer/_wix_uninstall.cmd
+++ installer/_wix_uninstall.cmd
@@ -5,8 +5,20 @@ pushd "%~dp0"
 
 set EXTENSIONS= ^
 WixToolset.Bal.wixext ^
+WixToolset.ComPlus.wixext ^
+WixToolset.Dependency.wixext ^
+WixToolset.DifxApp.wixext ^
+WixToolset.DirectX.wixext ^
+WixToolset.Firewall.wixext ^
+WixToolset.Http.wixext ^
+WixToolset.Iis.wixext ^
+WixToolset.Msmq.wixext ^
+WixToolset.Netfx.wixext ^
+WixToolset.PowerShell.wixext ^
+WixToolset.Sql.wixext ^
 WixToolset.UI.wixext ^
-WixToolset.Util.wixext
+WixToolset.Util.wixext ^
+WixToolset.VisualStudio.wixext
 
 for %%i in (%EXTENSIONS%) do (
   wix extension remove --global %%i
