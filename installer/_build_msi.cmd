05d52087325eea3832aea3c200ede4bd1f3609f6
nathancorvussolis <nathancorvussolis@gmail.com>
2024-07-25 20:40:49 +0900

    Specified the WindowsTargetPlatformVersion property in msbuild instead of vcxproj.

--- installer/_build_msi.cmd
+++ installer/_build_msi.cmd
@@ -13,16 +13,16 @@ if not defined SIGNCOMMAND set SignOutput=false
 set BUILDCOMMAND=dotnet build installer-msi.wixproj -nologo -verbosity:normal -target:Build
 
 echo build x86.msi
-%BUILDCOMMAND% -property:TargetPlatform=x86 -property:InstallerPlatform=x86 -property:BaseIntermediateOutputPath=%OutDir%\x86\
+%BUILDCOMMAND% -property:PackagePlatform=x86 -property:InstallerPlatform=x86 -property:BaseIntermediateOutputPath=%OutDir%\x86\
 
 echo build x64.msi
-%BUILDCOMMAND% -property:TargetPlatform=x64 -property:InstallerPlatform=x64 -property:BaseIntermediateOutputPath=%OutDir%\x64\
+%BUILDCOMMAND% -property:PackagePlatform=x64 -property:InstallerPlatform=x64 -property:BaseIntermediateOutputPath=%OutDir%\x64\
 
 echo build arm32.msi
-%BUILDCOMMAND% -property:TargetPlatform=arm32 -property:InstallerPlatform=arm64 -property:BaseIntermediateOutputPath=%OutDir%\arm32\
+%BUILDCOMMAND% -property:PackagePlatform=arm32 -property:InstallerPlatform=arm64 -property:BaseIntermediateOutputPath=%OutDir%\arm32\
 
 echo build arm64.msi
-%BUILDCOMMAND% -property:TargetPlatform=arm64 -property:InstallerPlatform=arm64 -property:BaseIntermediateOutputPath=%OutDir%\arm64\
+%BUILDCOMMAND% -property:PackagePlatform=arm64 -property:InstallerPlatform=arm64 -property:BaseIntermediateOutputPath=%OutDir%\arm64\
 
 popd
 endlocal
